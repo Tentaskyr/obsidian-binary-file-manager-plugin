a94b1c8f306b86f980c67183794faeb2c5a759ee
willjasen <brandon@willjasen.com>
2024-01-01 19:52:24 -0500

    fixing variable name

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-SOURCE_DIR=/Users/willjasen/GitHub/obsidian-binary-file-manager-plugin;
-PLUGIN_DIR=/Users/willjasen/Syncthing/Obsidian/willjasen/.obsidian/plugins/obsidian-binary-file-manager-plugin;
+SOURCE_DIR="/Users/willjasen/GitHub/obsidian-binary-file-manager-plugin";
+PLUGIN_DIR="/Users/willjasen/Syncthing/Obsidian/willjasen/.obsidian/plugins/obsidian-binary-file-manager-plugin";
 
 # Go to the source directory and build it
-cd $SOURCE_DIR;
+cd "$SOURCE_DIR";
 npm i;
 npm run build;
 
 # Copy built files into the plugin directory
-cp $SOURCE_DIR/main.js $PLUGN_DIR/main.js
-cp $SOURCE_DIR/manifest.js $PLUGN_DIR/manifest.js
-cp $SOURCE_DIR/styles.css $PLUGN_DIR/styles.css
+cp "$SOURCE_DIR/main.js" "$PLUGIN_DIR/main.js";
+cp $SOURCE_DIR/manifest.json $PLUGIN_DIR/manifest.json
+cp $SOURCE_DIR/styles.css $PLUGIN_DIR/styles.css
