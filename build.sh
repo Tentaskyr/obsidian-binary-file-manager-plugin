2dee737bc4ed508be11646a3eab92d3c40f2b923
willjasen <brandon@willjasen.com>
2024-01-01 20:01:09 -0500

    switched to array and for loop

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+# variables
 SOURCE_DIR="/Users/willjasen/GitHub/obsidian-binary-file-manager-plugin";
 PLUGIN_DIR="/Users/willjasen/Syncthing/Obsidian/willjasen/.obsidian/plugins/obsidian-binary-file-manager-plugin";
+FILES=( "main.js" "manifest.json" "styles.css" );
 
 # Go to the source directory and build it
-cd "$SOURCE_DIR";
-npm i;
-npm run build;
+cd $SOURCE_DIR;
+npm i; npm run build;
 
 # Copy built files into the plugin directory
-cp $SOURCE_DIR/main.js $PLUGIN_DIR/main.js;
-cp $SOURCE_DIR/manifest.json $PLUGIN_DIR/manifest.json;
-cp $SOURCE_DIR/styles.css $PLUGIN_DIR/styles.css;
+for file in "${FILES[@]}"
+do
+  echo "Copying file: $file";
+  cp $SOURCE_DIR/$file $PLUGIN_DIR/$file;
+done
