0819de55acb686d9a6ea54e36291c54817d21584
willjasen <brandon@willjasen.com>
2024-01-01 20:03:05 -0500

    comments

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# This script builds the plugin and copies the output files into the associated plugin directory
+# After the script is run, this plugin in Obisidian should disabled/enabled to refresh it
+
 # variables
 SOURCE_DIR="/Users/willjasen/GitHub/obsidian-binary-file-manager-plugin";
 PLUGIN_DIR="/Users/willjasen/Syncthing/Obsidian/willjasen/.obsidian/plugins/obsidian-binary-file-manager-plugin";
